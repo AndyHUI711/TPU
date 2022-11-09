e13a7af9e213a6cca214022278e87612a15849ca
AndyHUI711 <82593344+AndyHUI711@users.noreply.github.com>
2022-11-09 15:49:14 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,49 +1,71 @@
-#!/usr/bin/python3
+import argparse
+import cv2
+import os
 
-from os.path import join;
-from math import ceil;
-import numpy as np;
-import cv2;
-import tensorflow as tf;
+from pycoral.adapters.common import input_size
+from pycoral.adapters.detect import get_objects
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
 
-class Encoder(object):
+def main():
+    default_model_dir = '../all_models'
+    default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
+    default_labels = 'coco_labels.txt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='.tflite model path',
+                        default=os.path.join(default_model_dir,default_model))
+    parser.add_argument('--labels', help='label file path',
+                        default=os.path.join(default_model_dir, default_labels))
+    parser.add_argument('--top_k', type=int, default=3,
+                        help='number of categories with highest score to display')
+    parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
+    parser.add_argument('--threshold', type=float, default=0.1,
+                        help='classifier score threshold')
+    args = parser.parse_args()
 
-    def __init__(self, model_path = 'models'):
+    print('Loading {} with {} labels.'.format(args.model, args.labels))
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
+    labels = read_label_file(args.labels)
+    inference_size = input_size(interpreter)
 
-        self.model = tf.keras.models.load_model(join(model_path, 'facenet', '1'));
+    cap = cv2.VideoCapture(args.camera_idx)
 
-    def preprocess(self, img):
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2_im = frame
 
-        assert img.shape[2] == 3;
-        inputs = tf.expand_dims(img, axis = 0);
-        inputs = tf.cast(inputs, dtype = tf.float32);
-        mean = tf.math.reduce_mean(inputs, axis = [1,2,3]);
-        std = tf.math.maximum(
-            tf.math.sqrt(tf.math.reduce_mean(tf.math.pow(inputs - mean,2), axis = [1,2,3])),
-            1.0 / tf.math.sqrt(tf.cast(img.size, dtype = tf.float32))
-        );
-        inputs = (inputs - mean) / std;
-        outputs = tf.image.resize_with_pad(inputs, 160, 160);
-        return outputs;
+        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+        cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+        run_inference(interpreter, cv2_im_rgb.tobytes())
+        objs = get_objects(interpreter, args.threshold)[:args.top_k]
+        cv2_im = append_objs_to_img(cv2_im, inference_size, objs, labels)
 
-    def batch(self, imgs):
+        cv2.imshow('frame', cv2_im)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-        inputs = [self.preprocess(img) for img in imgs];
-        outputs = tf.concat(inputs, axis = 0);
-        return outputs;
+    cap.release()
+    cv2.destroyAllWindows()
 
-    def encode(self, imgs):
+def append_objs_to_img(cv2_im, inference_size, objs, labels):
+    height, width, channels = cv2_im.shape
+    scale_x, scale_y = width / inference_size[0], height / inference_size[1]
+    for obj in objs:
+        bbox = obj.bbox.scale(scale_x, scale_y)
+        x0, y0 = int(bbox.xmin), int(bbox.ymin)
+        x1, y1 = int(bbox.xmax), int(bbox.ymax)
 
-        assert type(imgs) is list;
-        if len(imgs) == 0: return tf.zeros((0,self.model.signatures['serving_default'].outputs[0].shape[-1]), dtype = tf.float32);
-        assert np.all([type(img) is np.ndarray and len(img.shape) == 3 for img in imgs]);
-        batch = self.batch(imgs);
-        feature = self.model.signatures['serving_default'](batch);
-        feature = feature['Bottleneck_BatchNorm'];
-        feature = feature / tf.math.sqrt(tf.math.reduce_sum(tf.math.pow(feature, 2), axis = -1, keepdims = True));
-        return feature;
+        percent = int(100 * obj.score)
+        label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
 
-if __name__ == "__main__":
+        cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+        cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+                             cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+    return cv2_im
 
-    assert tf.executing_eagerly() == True;
-    encoder = Encoder();
+if __name__ == '__main__':
+    main()
